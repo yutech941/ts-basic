02f398197eee0b190362b2503bd1075542315f8f
小林優一 <y.k94cofitec179@gmail.com>
2022-01-16 23:16:08 +0900

    環境構築とhtmlファイルを作成した。

---
+++ src/world.ts
@@ -0,0 +1,15 @@
+export default class World {
+    message: string;
+  
+    constructor(message: string) {
+      this.message = message
+    }
+  
+    public sayHello(elem: HTMLElement | null) {
+      if (elem) {
+        elem.innerText = this.message
+      }
+    }
+  }
+  
+  
