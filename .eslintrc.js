cb6319806501c497c6e163930278c3ae493a213e
小林優一 <y.k94cofitec179@gmail.com>
2022-01-23 18:37:56 +0900

    eslintとprettierの設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+ env: {
+     browser:true,
+     es6:true
+ },
+ extends: [
+     "eslint:recommended",
+     "plugin:@typescript-eslint/recommended",
+     "prettier",
+     "prettier/@typescript-eslint"
+ ],
+ plugins:[
+     "@typescript-eslint"
+ ],
+ parser:"@typescript-eslint/parser",
+ parserOptios: {
+     "sourceType": "module",
+     "project": "./tsconfig.json"
+ },
+ root:true,
+ rules:{}
+}
