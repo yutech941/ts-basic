2b778e06718298b1dda37a0dffa315c93518ab7c
小林優一 <y.k94cofitec179@gmail.com>
2022-01-23 18:54:36 +0900

    eslintの設定を合わせた。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,20 @@
 module.exports = {
- env: {
-     browser:true,
-     es6:true
- },
- extends: [
-     "eslint:recommended",
-     "plugin:@typescript-eslint/recommended",
-     "prettier",
-     "prettier/@typescript-eslint"
- ],
- plugins:[
-     "@typescript-eslint"
- ],
- parser:"@typescript-eslint/parser",
- parserOptios: {
-     "sourceType": "module",
-     "project": "./tsconfig.json"
- },
- root:true,
- rules:{}
-}
+    env: {
+      browser: true,
+      es6: true
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended", // TypeScriptでチェックされる項目をLintから除外する設定
+      "prettier", // prettierのextendsは他のextendsより後に記述する
+      "prettier/@typescript-eslint",
+    ],
+    plugins: ["@typescript-eslint"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      "sourceType": "module",
+      "project": "./tsconfig.json" // TypeScriptのLint時に参照するconfigファイルを指定
+    },
+    root: true, // 上位ディレクトリにある他のeslintrcを参照しないようにする
+    rules: {}
+  }
