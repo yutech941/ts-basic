e96a744a865d7403c03c9c1cf1a3909d82d4f524
小林優一 <y.k94cofitec179@gmail.com>
2022-01-25 22:12:29 +0900

    prettierの設定を加えた。

--- src/world.ts
+++ src/world.ts
@@ -1,15 +1,13 @@
 export default class World {
-    message: string;
-  
-    constructor(message: string) {
-      this.message = message
-    }
-  
-    public sayHello(elem: HTMLElement | null): void {
-      if (elem) {
-        elem.innerText = this.message
-      }
+  message: string
+
+  constructor(message: string) {
+    this.message = message
+  }
+
+  public sayHello(elem: HTMLElement | null): void {
+    if (elem) {
+      elem.innerText = this.message
     }
   }
-  
-  
+}
