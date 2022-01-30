f1a92b86821796b697c5b0b6abca9aab7dd2b9b1
小林優一 <y.k94cofitec179@gmail.com>
2022-01-30 12:12:37 +0900

    git commit

--- src/world.ts
+++ src/world.ts
@@ -5,7 +5,7 @@ export default class World {
     this.message = message
   }
 
-  public sayHello(elem: HTMLElement | null): void {
+  public sayHello(elem: HTMLElement | null) {
     if (elem) {
       elem.innerText = this.message
     }
