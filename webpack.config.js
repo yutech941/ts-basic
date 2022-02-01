325cd6c844778a2bb214f3337cb82d6f545e1d10
小林優一 <y.k94cofitec179@gmail.com>
2022-02-01 21:38:24 +0900

    webpackを修正

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,7 @@ module.exports = {
     extensions:['.ts','.js'] // Reactの.tsxや.jsxの拡張子も扱いたい場合は配列内に追加する
   },
   devServer: {
-    static: {
-        directory: path.join(__dirname, "dist"),
-      }, // webpack-dev-serverの公開フォルダ
+    contentBase: path.join(__dirname, 'dist'), // webpack-dev-serverの公開フォルダ
     open: true // サーバー起動時にブラウザを開く
   },
   // モジュールに適用するルールの設定（ローダーの設定を行う事が多い）
