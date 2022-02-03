5e2a679ee326271ef5af13980428b4d463d9b313
小林優一 <y.k94cofitec179@gmail.com>
2022-02-03 21:43:24 +0900

    プリミティブ型を作成した。

--- src/basic/primitive.ts
+++ src/basic/primitive.ts
@@ -1,3 +1,16 @@
 export default function primitiveSample(){
-    console.log("Foo")
+let name: string = '小林'
+//name = 123
+console.log("primitive sample1:",typeof name,name)
+
+let age: number = 28
+// age ="28"
+console.log("primitive sample 2:", typeof age,age)
+
+let isSingle: boolean = true
+//isSingle = "foo"
+console.log("primitive sample 3:", typeof isSingle,isSingle)
+
+const isOver20:boolean = age >= 20
+console.log("primitive sample 4:", typeof isOver20,isOver20)
 }
