dae87e9f69c09bebd7e56fb7a4a5f1dd60d57326
小林優一 <y.k94cofitec179@gmail.com>
2022-02-09 19:46:13 +0900

    関数のパラメータに型をつけた。

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,15 @@
 // const world = new World('Hello World!!!')
 // world.sayHello(root)
 
-//基本の型定義
-import {anySample, notExistSample, primitiveSample, unknownSample} from "./basic";
+// //基本の型定義
+// import {anySample, notExistSample, primitiveSample, unknownSample} from "./basic";
 
-anySample()
-notExistSample()
-primitiveSample()
-unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+// unknownSample()
+
+//関数の型定義
+import {logMessage} from "./function/basic"
+
+logMessage("Hello TypeScript!")
